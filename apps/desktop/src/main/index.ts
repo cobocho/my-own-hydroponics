6fdc33062b9d9e502e95f6bc67bfef7f028eb668
Kim Min-gyu <99083803+cobocho@users.noreply.github.com>
2025-05-06 18:23:18 +0900

    chore: ESLint 적용

--- apps/desktop/src/main/index.ts
+++ apps/desktop/src/main/index.ts
@@ -1,96 +1,90 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron';
-import { join } from 'path';
-import { electronApp, optimizer, is } from '@electron-toolkit/utils';
-import icon from '../../resources/icon.png?asset';
-import { SerialPort } from 'serialport';
-import { ReadlineParser } from '@serialport/parser-readline';
+/* eslint-disable promise/always-return */
+/* eslint-disable promise/catch-or-return */
+import { join } from 'path'
+
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { SerialPort } from 'serialport'
+import { ReadlineParser } from '@serialport/parser-readline'
+
+import icon from '../../resources/icon.png?asset'
 
 function createWindow(): void {
-	// Create the browser window.
-	const mainWindow = new BrowserWindow({
-		width: 900,
-		height: 670,
-		show: false,
-		autoHideMenuBar: true,
-		...(process.platform === 'linux' ? { icon } : {}),
-		webPreferences: {
-			preload: join(__dirname, '../preload/index.js'),
-			sandbox: false,
-		},
-	});
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false,
+    },
+  })
 
-	mainWindow.on('ready-to-show', () => {
-		mainWindow.show();
-	});
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
 
-	mainWindow.webContents.setWindowOpenHandler((details) => {
-		shell.openExternal(details.url);
-		return { action: 'deny' };
-	});
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
 
-	const port = new SerialPort({
-		path: '/dev/tty.SLAB_USBtoUART',
-		baudRate: 115200,
-	});
+  const port = new SerialPort({
+    path: '/dev/tty.SLAB_USBtoUART',
+    baudRate: 115200,
+  })
 
-	const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+  const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }))
 
-	port.on('open', () => {
-		console.log('open');
-	});
+  port.on('open', () => {
+    console.log('open')
+  })
 
-	parser.on('data', (data) => {
-		try {
-			const json = JSON.parse(data);
-			console.log(json);
-		} catch (error) {
-			console.error(error);
-		}
-	});
+  parser.on('data', (data: string) => {
+    try {
+      const json = JSON.parse(data) as {
+        temperature: number
+        humidity: number
+        light: number
+        water: number
+      }
+      console.log(json)
+    } catch (error) {
+      console.error(error)
+    }
+  })
 
-	// HMR for renderer base on electron-vite cli.
-	// Load the remote URL for development or the local html file for production.
-	if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-		mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
-	} else {
-		mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
-	}
+  if (is.dev && process.env.ELECTRON_RENDERER_URL) {
+    mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-	// Set app user model id for windows
-	electronApp.setAppUserModelId('com.electron');
+  electronApp.setAppUserModelId('com.electron')
 
-	// Default open or close DevTools by F12 in development
-	// and ignore CommandOrControl + R in production.
-	// see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-	app.on('browser-window-created', (_, window) => {
-		optimizer.watchWindowShortcuts(window);
-	});
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
 
-	// IPC test
-	ipcMain.on('ping', () => console.log('pong'));
+  // IPC test
+  ipcMain.on('ping', () => console.log('pong'))
 
-	createWindow();
+  createWindow()
 
-	app.on('activate', function () {
-		// On macOS it's common to re-create a window in the app when the
-		// dock icon is clicked and there are no other windows open.
-		if (BrowserWindow.getAllWindows().length === 0) createWindow();
-	});
-});
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
--- apps/desktop/src/preload/index.ts
+++ apps/desktop/src/preload/index.ts
@@ -12,11 +12,12 @@ if (process.contextIsolated) {
     contextBridge.exposeInMainWorld('electron', electronAPI)
     contextBridge.exposeInMainWorld('api', api)
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error(error)
   }
 } else {
-  // @ts-ignore (define in dts)
+  // @ts-expect-error (define in dts)
   window.electron = electronAPI
-  // @ts-ignore (define in dts)
+  // @ts-expect-error (define in dts)
   window.api = api
 }
